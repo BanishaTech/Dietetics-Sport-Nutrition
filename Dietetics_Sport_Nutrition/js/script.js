07e0b608dcb25315a967ebda57cb748cd0377f1f
BanishaTech <banishaofficial01@gmail.com>
2024-07-08 21:54:16 +0200

    Create new file and changes
    I add and create new file and delete

--- Dietetics_Sport_Nutrition/js/script.js
+++ Dietetics_Sport_Nutrition/js/script.js
@@ -1,20 +1,14 @@
+function showService(serviceId) {
+  const details = document.querySelectorAll('.service-detail');
 
-    document.addEventListener('DOMContentLoaded', function() {
-      const serviceLinks = document.querySelectorAll('.service-link');
-  
-      serviceLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-          event.preventDefault();
-  
-          const serviceId = this.getAttribute('href').substring(1);
-          const serviceDetails = document.querySelectorAll('.service-info');
-  
-          serviceDetails.forEach(info => {
-            info.classList.remove('active');
-          });
-  
-          const selectedService = document.getElementById(serviceId);
-          selectedService.classList.add('active');
-        });
-      });
-    });
+  // Ocultar todas las secciones de detalles
+  details.forEach(detail => {
+    detail.classList.add('hidden');
+  });
+
+  // Mostrar la sección de detalles seleccionada
+  const targetDetail = document.getElementById(serviceId);
+  if (targetDetail) {
+    targetDetail.classList.remove('hidden');
+  }
+}
