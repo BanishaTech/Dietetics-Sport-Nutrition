c9023b1de01ac76abab327c76b3bff03fb5731db
Banisha Shrestha <banishaofficial01@gmail.com>
2024-07-14 23:03:50 +0200

    Added Components Footer and Header
    I've created components and injected in HTML with JS

---
+++ Dietetics_Sport_Nutrition/js/addFooter.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+fetch("components/footer.html")
+  .then(response => response.text())
+  .then(stringResponse => {
+    const domParser = new DOMParser();
+    const footerDocument = domParser.parseFromString(stringResponse, 'text/html');
+    console.log("Hola");
+    footerDocument.querySelectorAll('link[rel="stylesheet"]').forEach(link => {
+      document.head.appendChild(link.cloneNode(true));
+    });
+
+    const footerContent = footerDocument.querySelector('footer');
+    if (footerContent) {
+      document.querySelector('footer').innerHTML = footerContent.innerHTML;
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  });
+});
---
+++ Dietetics_Sport_Nutrition/js/addHeader.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+fetch("components/header.html")
+  .then(response => response.text())
+  .then(stringResponse => {
+    const domParser = new DOMParser();
+    const headerDocument = domParser.parseFromString(stringResponse, 'text/html');
+    
+    headerDocument.querySelectorAll('link[rel="stylesheet"]').forEach(link => {
+      document.head.appendChild(link.cloneNode(true));
+    });
+
+    const headerContent = headerDocument.querySelector('header');
+    if (headerContent) {
+      document.querySelector('header').innerHTML = headerContent.innerHTML;
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  });
+});
